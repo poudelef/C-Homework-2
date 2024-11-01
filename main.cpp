ac10b4970646f267221fbd8c8db7b0d627eb7caa
shsm0520 <66741119+shsm0520@users.noreply.github.com>
2024-11-01 19:03:59 -0400

    Update main.cpp
    change applied for use 0 for inf

--- main.cpp
+++ main.cpp
@@ -8,13 +8,26 @@ int main()
     // Seed random number generator for card shuffling
     srand(static_cast<unsigned int>(time(0)));
 
-    std::cout << "Enter 1 to play until one player runs out of cards, or enter number of rounds: ";
+    std::cout << "Enter 0 to play until one player runs out of cards, or enter number of rounds: ";
     std::string input;
     std::cin >> input;
-    int rounds = (input == "1") ? -1 : std::stoi(input);
+
+    int rounds;
+
+    if (input == "0") {
+        rounds = -1;
+    }
+    else {
+        rounds = std::stoi(input);
+    }
+
 
     WarGame game(rounds);
     game.play_game();
+
+    std::cout << "Press Enter to exit...";
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+    std::cin.get(); 
 
     return 0;
 }
